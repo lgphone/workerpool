b9b1c00042af8306ebf337cff463493e60a87610
lgphone <inboxcvt@gmail.com>
2021-08-03 21:31:09 +0800

    更改命名

--- workerpool.go
+++ workerpool.go
@@ -10,26 +10,26 @@ type workerPool struct {
 	errs  []error
 }
 
-func (s *workerPool) Submit(fn func()) {
+func (w *workerPool) Submit(fn func()) {
 	go func() {
 		defer func() {
 			if err := recover(); err != nil {
-				s.mutex.Lock()
-				s.errs = append(s.errs, err.(error))
-				s.mutex.Unlock()
+				w.mutex.Lock()
+				w.errs = append(w.errs, err.(error))
+				w.mutex.Unlock()
 			}
-			<-s.ch
+			<-w.ch
 		}()
-		s.ch <- true
+		w.ch <- true
 		fn()
 	}()
 }
 
-func (s *workerPool) Wait() []error {
+func (w *workerPool) Wait() []error {
 	for {
-		if len(s.ch) == 0 {
-			close(s.ch)
-			return s.errs
+		if len(w.ch) == 0 {
+			close(w.ch)
+			return w.errs
 		}
 	}
 }
